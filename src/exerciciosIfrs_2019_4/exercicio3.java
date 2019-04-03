2ca736aea6c5ca430d7576c0fa10603ce97ca9fd
Grazielle <grazi.lemoss@gmail.com>
2019-04-03 16:36:37 -0300

    exercicio2 alterado e exercicio3 completo

---
+++ src/exerciciosIfrs_2019_4/exercicio3.java
@@ -0,0 +1,28 @@
+package exerciciosIfrs_2019_4;
+
+/*Declare um vetor de 10 posições e o preencha automaticamente com os 10
+primeiros números impares e o escreva.
+*/
+
+public class exercicio3 {
+	
+	public static void main (String[]args) {
+		
+		int [] i = new int [10];
+		i[0] = 1;
+		i[1] = 2;
+		i[2] = 3;
+		i[3] = 4;
+		i[4] = 5;
+		i[5] = 6;
+		i[6] = 7;
+		i[7] = 8;
+		i[8] = 9;
+		
+			System.out.println(i[0] + " " + i[2] + " " + i[4]+ " "+ i [6] + " "+ i[8]);
+			
+			}
+
+	}
+
+
